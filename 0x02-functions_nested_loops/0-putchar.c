c5d5c3ceeb55ab48567259936f94fa93c7bd9144
Milly Mwasame <millymwasame@example.com>
2023-09-12 13:07:28 -0700

    Prints Putchar then a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * main - Prints "_putcchar" followed by a new line.
- *
- * Return: Always 0.
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	char j;
-	for (i=0 ; i < 10 ; i++;
-	{
-		for (j = 'a' ; j <= 'z' ; i++)
-			_putchar(j);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
