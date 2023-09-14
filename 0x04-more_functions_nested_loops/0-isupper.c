5bdc6735b66ae720524c3ca3fb1e6787f3973fa5
Milly Mwasame <millymwasame@example.com>
2023-09-14 10:18:52 -0700

    Returns 1 if c is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*Checks for uppercase character
+ *Returns 1 if c is uppercase and 0 if otherwise*/
+
+int _isupper(int c)
+{
+    if (c >= 'A' c <= 'Z')
+	return(1);
+    else
+	return(0);
+}
