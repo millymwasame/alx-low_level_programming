877516df9022dfc83003ac135ed06aa9040ce31c
Milly Mwasame <millymwasame@example.com>
2023-09-26 06:39:22 -0700

    copies memories from one area to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memcpy: copies memory from one area to another
+ * @src: pointer to the char parameters
+ * @dest: pointer to the char parameters
+ *@n: size copied
+ * Return: *dest
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
